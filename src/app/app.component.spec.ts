085cc439f750a3cc77765198851c9e478640514d
Oleh Hrytsyshyn <oleh.hrytsyshyn@bytebrand.net>
2025-06-10 15:21:02 +0300

    new commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'drag-drop' title`, () => {
+  it(`should have the 'drag_drop' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('drag-drop');
+    expect(app.title).toEqual('drag_drop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, drag-drop');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, drag_drop');
   });
 });
